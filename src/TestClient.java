10a3f372e0f4fa0c347eba7435ac650f3d360257
Rajala1404 <post.rajan@schirmer-family.de>
2024-01-24 14:35:29 +0100

    Initial Commit

---
+++ src/TestClient.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class TestClient {
+    public static void main(String[] args) throws IOException {
+        DatagramPacket sendPacket;
+        byte[] sendData;
+        DatagramSocket clientSocket = new DatagramSocket();
+        clientSocket.setSoTimeout(1000);
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String cmd = input.nextLine();
+            if (cmd.equals("QUIT")) {
+                clientSocket.close();
+                System.exit(1);
+            }
+            sendData = cmd.getBytes();
+            sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("127.0.0.1"), Main.PORT);
+            clientSocket.send(sendPacket);
+        }
+    }
+}
